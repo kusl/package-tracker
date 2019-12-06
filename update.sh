a4afac88decfeb21a0e081ed79a27f3126e7fc4b
Kushal <kushaldeveloper@gmail.com>
2019-12-06 11:04:03 +0000

    add tracking

--- update.sh
+++ update.sh
@@ -3,4 +3,3 @@ curl 'https://www.ups.com/track/api/Track/GetStatus?loc=en_US' -H 'Accept: appli
 curl 'https://www.ups.com/track/api/Track/GetStatus?loc=en_US' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-Type: application/json' --data '{"Locale":"en_US","TrackingNumber":["1Z2W9W130306560243"],"Requester":"null"}' > ups1Z2W9W130306560243.json;
 git add .;
 git commit -m "add tracking";
-git push origin master;`
